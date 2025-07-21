be96875703e199f8c7a548a7714651d4a633c909
Bernardo Resende (xWF) <deresendepinto@google.com>
2025-07-21 18:54:40 +0000

    That's great! but now we're missing the team card delete icon when hover

--- src/components/teams/team-management.tsx
+++ src/components/teams/team-management.tsx
@@ -248,7 +248,7 @@ function TeamCard({
     };
 
     return (
-        <Card className="flex flex-col h-full bg-transparent relative" {...props}>
+        <Card className="group flex flex-col h-full bg-transparent relative" {...props}>
              {!isSharedPreview && (
                 <TooltipProvider>
                     <Tooltip>
