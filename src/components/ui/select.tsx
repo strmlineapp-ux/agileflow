a3dc80b93291dceea911ca0754d5dccd7e5ff49e
Bernardo Resende (xWF) <deresendepinto@google.com>
2025-07-24 09:33:17 +0000

    It hasn't changed the inconsistent focus ring styles, unfortunately. Thi

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,5 +1,4 @@
 
-
 "use client"
 
 import * as React from "react"
@@ -21,7 +20,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus-visible:ring-1 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
     {...props}
