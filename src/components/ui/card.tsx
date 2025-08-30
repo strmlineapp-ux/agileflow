9ea3e084b7cd7f01e2c171d32a84374f73bae432
Strm Line <strm.line.app@gmail.com>
2025-08-30 23:59:18 +0000

    I can't see the changes in the card or the sidebar menu borders.

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,7 +10,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border border-border bg-card text-foreground",
+      "rounded-lg border bg-card text-foreground",
       className
     )}
     {...props}
