8244d20baf0dbd1b64f4518b78f99c8640e64238
Strm Line <strm.line.app@gmail.com>
2025-08-30 15:55:28 +0000

    Yes, please.

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -40,6 +40,7 @@ export default {
         },
         muted: {
           DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+          /** The default color for less important text. */
           foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
         },
         accent: {
@@ -56,7 +57,6 @@ export default {
         },
         card: {
           DEFAULT: "hsl(var(--card) / <alpha-value>)",
-          foreground: "hsl(var(--card-foreground) / <alpha-value>)",
         },
       },
       borderRadius: {
