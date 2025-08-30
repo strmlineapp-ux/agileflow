9e6f865452a48f51437c45e261da0296d028b54f
Strm Line <strm.line.app@gmail.com>
2025-08-30 21:51:50 +0000

    Active State is wrong, it should use emphasis logic for the selected tab

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -34,8 +34,8 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-none data-[state=active]:bg-transparent",
-      "font-emphasis data-[state=active]:text-primary",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-transparent",
+      "font-emphasis",
       className
     )}
     {...props}
