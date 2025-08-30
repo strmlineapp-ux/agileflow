e0918a00b8c3bbb2c5fb60a891b0fe4100314c6c
Strm Line <strm.line.app@gmail.com>
2025-08-30 21:02:42 +0000

    Does the link variant follow the emphasis logic and become underlined on

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         default: "hover:bg-transparent",
         outline:
           "border border-input bg-transparent",
-        link: "text-primary underline-offset-4",
+        link: "text-primary underline-offset-4 hover:underline",
         circle: "rounded-full",
       },
       size: {
