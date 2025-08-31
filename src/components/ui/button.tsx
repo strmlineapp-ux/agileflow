cf6312bc8d3cb2437e87ad3449434aa01798ff11
Strm Line <strm.line.app@gmail.com>
2025-08-31 15:38:56 +0000

    It didn't work are there any styles applied to these buttons?

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         default: "text-foreground",
         destructive: "bg-destructive text-destructive-foreground",
         outline: "border border-input bg-transparent",
-        ghost: "hover:bg-transparent hover:text-primary",
+        ghost: "hover:bg-transparent",
         circle: "rounded-full",
       },
       size: {
