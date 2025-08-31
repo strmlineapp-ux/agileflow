6e81d64b75684e66fce3284ba5840658dced2c87
Strm Line <strm.line.app@gmail.com>
2025-08-31 11:56:09 +0000

    Can we set the separator opacity to 40%

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -22,7 +22,7 @@ const TabsList = React.forwardRef<
       )}
       {...props}
     />
-    <Separator className="absolute -bottom-px bg-border" />
+    <Separator className="absolute -bottom-px bg-border/40" />
   </div>
 ))
 TabsList.displayName = TabsPrimitive.List.displayName
