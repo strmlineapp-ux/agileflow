0f7b2211e699dbaac58fba9d39c533b208b85a9f
Strm Line <strm.line.app@gmail.com>
2025-08-31 22:36:37 +0000

    I think the issue with borders in the dark theme is that the components

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -1,4 +1,3 @@
-
 import type {Config} from "tailwindcss";
 
 export default {
@@ -21,8 +20,8 @@ export default {
         body: ['"Roboto"', "sans-serif"],
       },
       colors: {
-        border: "hsl(var(--border) / 0)",
-        input: "hsl(var(--input) / 0)",
+        border: "hsl(var(--border) / <alpha-value>)",
+        input: "hsl(var(--input) / <alpha-value>)",
         ring: "hsl(var(--ring) / <alpha-value>)",
         background: "hsl(var(--background) / <alpha-value>)",
         foreground: "hsl(var(--foreground) / <alpha-value>)",
