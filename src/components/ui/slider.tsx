6c7b4f58016ebb7a4445e2034b31ecf25d11588f
Strm Line <strm.line.app@gmail.com>
2025-08-31 17:51:43 +0000

    Set the Track height to h-1

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -18,7 +17,7 @@ const Slider = React.forwardRef<
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-foreground">
+    <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-foreground">
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-foreground bg-card ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
