b85652fce5fd3eba2fe09caafdc43d4a30ad722d
Strm Line <strm.line.app@gmail.com>
2025-08-31 10:19:03 +0000

    Okay, so the issue is that all borders are too thick. the separator is f

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -20,8 +20,8 @@ export default {
         emphasis: "var(--font-weight-emphasis, 500)",
       },
       colors: {
-        border: "hsl(var(--foreground) / 0.4)",
-        input: "hsl(var(--foreground) / 0.4)",
+        border: "hsl(var(--foreground) / <alpha-value>)",
+        input: "hsl(var(--foreground) / <alpha-value>)",
         ring: "hsl(var(--primary) / <alpha-value>)",
         background: "hsl(var(--background) / <alpha-value>)",
         foreground: "hsl(var(--foreground) / <alpha-value>)",
