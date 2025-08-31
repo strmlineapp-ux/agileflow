d4d1ea394452962880611e6d8c8b21a3af5c35b6
Strm Line <strm.line.app@gmail.com>
2025-08-31 17:28:26 +0000

    The slider main container background is white.

--- src/components/settings/user-management.tsx
+++ src/components/settings/user-management.tsx
@@ -18,7 +18,6 @@ import { CompactSearchInput } from '../common/compact-search-input';
 import { HslStringColorPicker } from 'react-colorful';
 import { Slider } from '../ui/slider';
 import { UserCard } from '@/components/common/user-card';
-import { TransparentCard, TransparentCardContent } from '../ui/transparent-card';
 
 const predefinedColors = [
     'hsl(0, 84%, 60%)', 'hsl(25, 95%, 53%)', 'hsl(45, 93%, 47%)', 'hsl(88, 62%, 53%)', 'hsl(142, 71%, 45%)', 'hsl(160, 100%, 37%)',
@@ -325,9 +324,7 @@ function CurrentUserCard({ user, isCurrentUser, canEditPreferences, className }:
                                   </TooltipContent>
                                 </Tooltip>
                               </TooltipProvider>
-                              <PopoverContent className="w-48" asChild>
-                                <TransparentCard>
-                                  <TransparentCardContent>
+                              <PopoverContent className="w-48 p-4 bg-transparent border-none shadow-none">
                                     <Slider
                                       value={[currentWeightIndex]}
                                       onValueChange={handleFontWeightChange}
@@ -335,8 +332,6 @@ function CurrentUserCard({ user, isCurrentUser, canEditPreferences, className }:
                                       max={4}
                                       step={1}
                                     />
-                                  </TransparentCardContent>
-                                </TransparentCard>
                               </PopoverContent>
                             </Popover>
 
@@ -355,12 +350,8 @@ function CurrentUserCard({ user, isCurrentUser, canEditPreferences, className }:
                                         </TooltipContent>
                                     </Tooltip>
                                 </TooltipProvider>
-                                <PopoverContent className="w-48" asChild>
-                                    <TransparentCard>
-                                        <TransparentCardContent>
-                                            <Slider value={[currentIconGradeIndex]} onValueChange={handleIconGradeChange} min={0} max={2} step={1} />
-                                        </TransparentCardContent>
-                                    </TransparentCard>
+                                <PopoverContent className="w-48 p-4 bg-transparent border-none shadow-none">
+                                    <Slider value={[currentIconGradeIndex]} onValueChange={handleIconGradeChange} min={0} max={2} step={1} />
                                 </PopoverContent>
                             </Popover>
                             
@@ -379,12 +370,8 @@ function CurrentUserCard({ user, isCurrentUser, canEditPreferences, className }:
                                         </TooltipContent>
                                     </Tooltip>
                                 </TooltipProvider>
-                                <PopoverContent className="w-48" asChild>
-                                  <TransparentCard>
-                                    <TransparentCardContent>
-                                      <Slider value={[currentIconOpticalSizeIndex]} onValueChange={handleIconOpticalSizeChange} min={0} max={3} step={1} />
-                                    </TransparentCardContent>
-                                  </TransparentCard>
+                                <PopoverContent className="w-48 p-4 bg-transparent border-none shadow-none">
+                                  <Slider value={[currentIconOpticalSizeIndex]} onValueChange={handleIconOpticalSizeChange} min={0} max={3} step={1} />
                                 </PopoverContent>
                             </Popover>
 
@@ -403,12 +390,8 @@ function CurrentUserCard({ user, isCurrentUser, canEditPreferences, className }:
                                         </TooltipContent>
                                     </Tooltip>
                                 </TooltipProvider>
-                                <PopoverContent className="w-48" asChild>
-                                  <TransparentCard>
-                                    <TransparentCardContent>
-                                      <Slider value={[currentRadius * 10]} onValueChange={handleRadiusChange} min={0} max={15} step={1} />
-                                    </TransparentCardContent>
-                                  </TransparentCard>
+                                <PopoverContent className="w-48 p-4 bg-transparent border-none shadow-none">
+                                  <Slider value={[currentRadius * 10]} onValueChange={handleRadiusChange} min={0} max={15} step={1} />
                                 </PopoverContent>
                             </Popover>
 
