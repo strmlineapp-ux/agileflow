ac4d3d70a349c9eb84b74e3ad206dd892da39c51
Strm Line <strm.line.app@gmail.com>
2025-08-31 14:30:33 +0000

    I can't see the app logo anymore, there's a space for it at the top of t

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -12,6 +12,7 @@ import { useUser } from '@/context/user-context';
 import { GoogleSymbol } from '../icons/google-symbol';
 import { ScrollArea } from '../ui/scroll-area';
 import { hasAccess } from '@/lib/permissions';
+import Logo from '../icons/logo';
 
 
 export function Sidebar() {
@@ -48,13 +49,12 @@ export function Sidebar() {
   return (
     <aside className="fixed inset-y-0 left-0 z-40 hidden w-14 flex-col border-r bg-card sm:flex">
         <nav className="flex flex-col items-center gap-4 px-2 pt-4 pb-2">
-            <Link href="/dashboard/overview" className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg md:h-8 md:w-8 md:text-base">
-                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ color: 'hsl(210 20% 95%)' }}>
-                <path d="M12 2L2 7L12 12L22 7L12 2Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M2 17L12 22L22 17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M2 12L12 17L22 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                <span className="sr-only">AgileFlow</span>
+            <Link
+              href="/dashboard/overview"
+              className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg text-primary-foreground md:h-8 md:w-8 md:text-base"
+            >
+              <Logo className="text-primary-foreground" />
+              <span className="sr-only">AgileFlow</span>
             </Link>
             {adminPage && hasAccess(viewAsUser, adminPage) && adminPage.path && (
               <TooltipProvider>
