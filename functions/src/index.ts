d4816d82a00fe641c57a28a4a5e4cf36015953e3
Strm Line <strm.line.app@gmail.com>
2025-09-02 19:45:52 +0000

    Shouldn't we stick to the modern ES Module and run npm install as sugges

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,6 @@ admin.initializeApp();
 
 // This is the single entry point for all functions.
 // We export all the functions from their individual files here.
-export * from './user-management';
-export * from './send-invitation';
-export * from './calendar-webhook';
+export * from './user-management.js';
+export * from './send-invitation.js';
+export * from './calendar-webhook.js';
