11f74d804ebceabb5a29200ab009660a835928b5
Strm Line <strm.line.app@gmail.com>
2025-09-03 21:09:32 +0000

    And do we have any remaining focus ring definitions? Or any components t

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -8,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md bg-transparent px-3 py-2 ring-offset-background file:bg-transparent file:text-sm placeholder:text-foreground/70 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md bg-transparent px-3 py-2 ring-offset-background file:bg-transparent file:text-sm placeholder:text-foreground/70 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
