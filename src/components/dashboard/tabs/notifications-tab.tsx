785e64421c7db79320743913a3a3777e91f41fd8
Strm Line <strm.line.app@gmail.com>
2025-09-04 22:46:01 +0000

    I noticed that  if I move the tab close to the top, the gap is shorter.

--- src/components/dashboard/tabs/notifications-tab.tsx
+++ src/components/dashboard/tabs/notifications-tab.tsx
@@ -33,7 +33,7 @@ export function NotificationsContent({ page, tab }: { page?: AppPage, tab?: AppT
   };
 
   return (
-    <div className="flex flex-col h-full gap-6">
+    <div className="h-full">
       <PageTitle 
         title={title}
         onSave={handleTitleSave}
