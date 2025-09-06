e051b3ef03cef8b8adfd43cdf651624021d058a1
Strm Line <strm.line.app@gmail.com>
2025-09-06 01:19:48 +0000

    These scrollbars are still visible.

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -37,7 +37,6 @@ const ScrollBar = React.forwardRef<
         "h-full w-2.5 border-l border-l-transparent p-[1px]",
       orientation === "horizontal" &&
         "h-2.5 flex-col border-t border-t-transparent p-[1px]",
-      "invisible", // This will hide the scrollbar but keep functionality
       className
     )}
     {...props}
