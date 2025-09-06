b43c2bb8d2a752cbef6ccdbdc43b0585a69f4e34
Strm Line <strm.line.app@gmail.com>
2025-09-06 01:26:40 +0000

    In the calendar grid, the current day uses the primary colour in the bac

--- src/components/calendar/month-view.tsx
+++ src/components/calendar/month-view.tsx
@@ -119,7 +119,7 @@ export const MonthView = React.memo(({ date, events, containerRef, onEventClick 
             )}>
                 <span className={cn(
                     "h-6 w-6 flex items-center justify-center rounded-full text-sm text-foreground",
-                    { "bg-primary text-primary-foreground": isDayToday },
+                    { "font-emphasis": isDayToday },
                     { "text-foreground/50": day.getMonth() !== date.getMonth() },
                     { "text-foreground/50": (isWeekend || isDayHoliday) }
                 )}>
