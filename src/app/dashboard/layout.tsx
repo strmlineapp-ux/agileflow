230c9931b7850312de5ea89d65f515479384068e
Strm Line <strm.line.app@gmail.com>
2025-09-06 19:29:34 +0000

    I can't drag the cards to the shared panel. You said this library was pe

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -49,11 +49,11 @@ export default function DashboardLayout({
   }
 
   return (
-    <div className="min-h-screen w-full bg-background overflow-hidden">
+    <div className="min-h-screen w-full bg-background">
       <Sidebar />
       <div className="flex flex-col sm:pl-14 h-screen">
         <Header />
-        <main className="flex-1 flex flex-col p-4 sm:p-6 overflow-hidden">
+        <main className="flex-1 flex flex-col p-4 sm:p-6 min-h-0">
           {children}
         </main>
       </div>
