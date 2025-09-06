b8f675219bf92f1606bf95ac4b4a1b1539d1ac09
Strm Line <strm.line.app@gmail.com>
2025-09-06 20:11:45 +0000

    I still can't  scroll the calendars.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -54,7 +54,7 @@ export default function DashboardLayout({
       <Sidebar />
       <div className="flex flex-col sm:pl-14 h-screen">
         <Header />
-        <main className="flex-1 flex flex-col p-4 sm:p-6">
+        <main className="flex-1 flex flex-col p-4 sm:p-6 overflow-y-auto">
           {children}
         </main>
       </div>
