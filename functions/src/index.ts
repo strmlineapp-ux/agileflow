fae29d3ae0151bfc775fe01894daf187049c412c
Strm Line <strm.line.app@gmail.com>
2025-09-07 20:17:34 +0000

    firebase deploy --only functions
    === Deploying to 'agileflow-mlf18'...

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,5 +6,5 @@ admin.initializeApp();
 
 // Export functions from their individual files
 export { onNewUserCreated } from './user-management.js';
-export { sendInvitationTrigger } from './send-invitation.js';
+export { onSendInvitation } from './send-invitation.js';
 export { calendarWebhook } from './calendar-webhook.js';
